b4f0fcf80a937d13b004a35b8b2823781c488393
genrich-grents <genrich623@gmail.com>
2016-01-24 16:21:01 +0300

    fixed links in letters for production

--- app/mailers/request_mailer.rb
+++ app/mailers/request_mailer.rb
@@ -1,6 +1,7 @@
 class RequestMailer < ApplicationMailer
   def send_mail(request)
     @request = request
+    @host = (Rails.env == 'production') ? 'testify-test.herokuapp.com' : '0.0.0.0:3000'
     mail(from: 'noreply@testify-test.herokuapp.com', to: request.email,
          subject: request.subject, template_name: 'request')
   end
